ee9a587a4cdb6cfdc4c4198d1ad6131e1e2fecba
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-13 12:48:32 -0400

    Made createCard function and used map() to create new cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,25 @@ import Card from "./Card";
 import Avatar from "./Avatar";
 import contacts from "./contacts";
 
+function createCard(contact) {
+  return (
+    <Card
+      name={contact.name}
+      img={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 function App() {
   return (
     <div>
       <h1 className="heading">✨ Magic Cat Buddies ✨</h1>
       <Avatar img="https://cdn.shopify.com/s/files/1/1365/2497/products/wizard_cat_1_800x.jpg" />
-      <Card
+
+      {contacts.map(createCard)}
+
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         alt="avatar_img"
@@ -28,7 +41,7 @@ function App() {
         alt="avatar_img"
         phone={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,12 +1,14 @@
 const contacts = [
   {
-    name: "Bee",
+    id: 1,
+    name: "Puppycat",
     imgURL:
       "https://frederatorstudios.com/app/uploads/2018/01/puppycat-icon-550x550.jpg",
     phone: "+123 456 789",
     email: "puppycat@2cute2poot.com"
   },
   {
+    id: 2,
     name: "Catbug",
     imgURL:
       "https://pbs.twimg.com/profile_images/523174422812037120/aLIBOTmj.jpeg",
@@ -14,6 +16,7 @@ const contacts = [
     email: "catbug@interdimensional.com"
   },
   {
+    id: 3,
     name: "Luna",
     imgURL:
       "https://www.pngitem.com/pimgs/m/74-747238_moon-png-myedit-image-luna-sailor-moon-png.png",
