c65fb11c8cdb7cf02d35de756d6d331b5d210fc0
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-13 09:21:12 -0400

    Complete card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,12 @@ function Card(props) {
   return (
     <div className="card">
       <div className="top">
-        <h2>{props.name}</h2>
-        <img src={props.img} alt={props.alt} />
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src={props.img} alt={props.alt} />
       </div>
       <div className="bottom">
-        <p>{props.phone}</p>
-        <p>{props.email}</p>
+        <p className="info">{props.phone}</p>
+        <p className="info">{props.email}</p>
       </div>
     </div>
   );
